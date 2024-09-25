b27107e1ce52de471800f0ce4f3be2a8ad83a1e3
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-25 15:49:59 +0300

    app

--- src/js/films-api.js
+++ src/js/films-api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDlmZDM3OWFlYTAxYjdjMTg1MTk3NTIyMDIyZWFmZCIsIm5iZiI6MTcyNzA5Nzc1OC4wNzg3OTYsInN1YiI6IjY2ZjE0Nzc0YTgyYjAwNTcwMzI2ODU4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9fqiYuQg8jLiuqFuD-K3fKbnC6uhKuwMW1BkX40fw88";
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDlmZDM3OWFlYTAxYjdjMTg1MTk3NTIyMDIyZWFmZCIsIm5iZiI6MTcyNzI2NjY4Ni4yOTEwODksInN1YiI6IjY2ZjE0Nzc0YTgyYjAwNTcwMzI2ODU4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-GLrnY5XYsN15cGCySBfSMG74P_T1yaTh9IgcS2s7GA";
 
 export const getFilmsTrendingAccess = async () => {
-  const url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US`;
+  const url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
   const params = {
     headers: {
-      Authorization: `Bearer ${TOKEN}`,
+      Authorization: "Bearer ${TOKEN}",
     },
   };
   try {
@@ -18,10 +18,11 @@ export const getFilmsTrendingAccess = async () => {
 };
 
 export const getFilmsDetails = async (id, codeWord = "") => {
-  const url = `https://api.themoviedb.org/3/movie/${id}${codeWord}?language=en-US`;
+  const url =
+    "https://api.themoviedb.org/3/movie/${id}${codeWord}?language=en-US";
   const params = {
     headers: {
-      Authorization: `Bearer ${TOKEN}`,
+      Authorization: "Bearer ${TOKEN}",
     },
   };
   try {
@@ -34,10 +35,11 @@ export const getFilmsDetails = async (id, codeWord = "") => {
 };
 
 export const getFilmsSearch = async (query, page = 1) => {
-  const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`;
+  const url =
+    "https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${page}";
   const params = {
     headers: {
-      Authorization: `Bearer ${TOKEN}`,
+      Authorization: "Bearer ${TOKEN}",
     },
   };
   try {
