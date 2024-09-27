d75215e60f2f9e06a1991db74aafc548daf16925
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-27 14:16:04 +0300

    canvas

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,107 +1,208 @@
-// //Forked by https://codepen.io/giana/pen/qbWNYy
-// "use strict";
+(function() {
+    "use strict";
+/* This is a total mess, sorry. Will clean up eventually */
 
-// var canvas = document.getElementById("canvas"),
-//   ctx = canvas.getContext("2d"),
-//   w = (canvas.width = window.innerWidth),
-//   h = (canvas.height = window.innerHeight),
-//   hue = 217,
-//   stars = [],
-//   count = 0,
-//   maxStars = 1400;
+    // Canvas things
+    var canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d'),
+        canvasWidth = canvas.width = window.innerWidth,
+        canvasHeight = canvas.height = window.innerHeight;
 
-// // Thanks @jackrugile for the performance tip! https://codepen.io/jackrugile/pen/BjBGoM
-// // Cache gradient
-// var canvas2 = document.createElement("canvas"),
-//   ctx2 = canvas2.getContext("2d");
-// canvas2.width = 100;
-// canvas2.height = 100;
-// var half = canvas2.width / 2,
-//   gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
-// gradient2.addColorStop(0.025, "#fff");
-// gradient2.addColorStop(0.1, "hsl(" + hue + ", 61%, 33%)");
-// gradient2.addColorStop(0.25, "hsl(" + hue + ", 64%, 6%)");
-// gradient2.addColorStop(1, "transparent");
+    // Mouse things
+    var mouseX,
+        mouseY,
+        pop = false,
+        attract = false;
 
-// ctx2.fillStyle = gradient2;
-// ctx2.beginPath();
-// ctx2.arc(half, half, half, 0, Math.PI * 2);
-// ctx2.fill();
+    // Check if mouse event is over a bubble
+    var mouseOver = function(x, y, radius) {
+        var diffX = mouseX - x;
+        var diffY = mouseY - y;
 
-// // End cache
+        if (diffX < radius && diffX > (radius * -1) && diffY < radius && diffY > (radius * -1)) {
+            return true;
+        }
 
-// function random(min, max) {
-//   if (arguments.length < 2) {
-//     max = min;
-//     min = 0;
-//   }
+        return false;
+    }
 
-//   if (min > max) {
-//     var hold = max;
-//     max = min;
-//     min = hold;
-//   }
+    // Used for randomizing everything
+    var randomNum = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
+    // Used for changing settings with a random number
+    var changeSettings = function(setting, min, max, prob) {
+        var chance = randomNum(0, prob);
 
-// function maxOrbit(x, y) {
-//   var max = Math.max(x, y),
-//     diameter = Math.round(Math.sqrt(max * max + max * max));
-//   return diameter / 2;
-// }
+        if(setting < min || chance === 1) {
+            return 1;
+        } else if (setting > max || chance === 2) {
+            return -1;
+        } else {
+            return 0;
+        }
+    };
 
-// var Star = function () {
-//   this.orbitRadius = random(maxOrbit(w, h));
-//   this.radius = random(60, this.orbitRadius) / 12;
-//   this.orbitX = w / 2;
-//   this.orbitY = h / 2;
-//   this.timePassed = random(0, maxStars);
-//   this.speed = random(this.orbitRadius) / 50000;
-//   this.alpha = random(2, 10) / 10;
+    // Bubble config
+    var bubbles = [], // Holds all the bubbles as objects
+        count = 0, // Bubble count
+        maxCount = 60, // Max bubbles to render on start
+        minSize = 5,
+        maxSize = 15,
+        minSpeed = 10,
+        maxSpeed = 40,
+        bgcolor = 'hsla(240,50%,9%,0.2)', // Canvas bg
+        colors = [ // Color palette
+           // { color1 : 'hsl(60,100%,75%)', color2 : 'hsl(10,100%,30%)'},
+            { color1 : 'hsl(120,100%,75%)', color2 : 'hsl(10,100%,30%)'},
+            { color1 : 'hsl(155,100%,75%)', color2 : 'hsl(10,100%,30%)'},
+            { color1 : 'hsl(180,100%,75%)', color2 : 'hsl(10,100%,30%)'},
+            { color1 : 'hsl(360,100%,75%)', color2 : 'hsl(10,100%,30%)'},
+          
+            { color1 : 'hsl(10,100%,52%)', color2 : 'hsl(55,100%,72% )'},
+            { color1 : 'hsl(180,100%,75%)',color2 : 'hsl(120,100%,72%)'},
+            { color1 : 'hsl(60,100%,75%)', color2 : 'hsl(155,100%,65%)'},
+            { color1 : 'hsl(10,100%,52%)', color2 : 'hsl(180,100%,52%)'}
+        ];
 
-//   count++;
-//   stars[count] = this;
-// };
+    // Bubble constructor
+    var Bubble = function(x, y, size) {
+        this.id = count+1;
+        this.x = x || randomNum(0, canvasWidth);
+        this.y =  y || randomNum(0, canvasHeight);
+        this.radius = size || randomNum(minSize, maxSize);
+        this.color = colors[randomNum(0,colors.length-1)];
 
-// Star.prototype.draw = function () {
-//   var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
-//     y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
-//     twinkle = random(10);
+        this.speed = randomNum(minSpeed, maxSpeed)/10;
+        this.speedBackup = this.speed;
+        this.directionX = randomNum(-1,1) || 1;
+        this.directionY = randomNum(-1,1) || 1;
+        this.flicker = 0;
 
-//   if (twinkle === 1 && this.alpha > 0) {
-//     this.alpha -= 0.05;
-//   } else if (twinkle === 2 && this.alpha < 1) {
-//     this.alpha += 0.05;
-//   }
+        count++; // Number bubbles
+        bubbles[count] = this; // Add to main object
+    };
 
-//   ctx.globalAlpha = this.alpha;
-//   ctx.drawImage(
-//     canvas2,
-//     x - this.radius / 2,
-//     y - this.radius / 2,
-//     this.radius,
-//     this.radius
-//   );
-//   this.timePassed += this.speed;
-// };
+    // Bubble drawing animation
+    Bubble.prototype.draw = function() {
 
-// for (var i = 0; i < maxStars; i++) {
-//   new Star();
-// }
+        // Change direction randomly, default to same direction
+        this.directionX = changeSettings(this.x, 0, canvasWidth, 500) || this.directionX;
+        this.directionY = changeSettings(this.y, 0, canvasHeight, 500) || this.directionY;
 
-// function animation() {
-//   ctx.globalCompositeOperation = "source-over";
-//   ctx.globalAlpha = 0.8;
-//   ctx.fillStyle = "hsla(" + hue + ", 64%, 6%, 1)";
-//   ctx.fillRect(0, 0, w, h);
+        // Reset speed
+        this.speed = this.speedBackup;
 
-//   ctx.globalCompositeOperation = "lighter";
-//   for (var i = 1, l = stars.length; i < l; i++) {
-//     stars[i].draw();
-//   }
+        // If mouse is held down & bubble is within 200px of mouse
+        if (attract === true && mouseOver(this.x,this.y,200)) {
+            var moveTowardMouse = randomNum(0,15); // Chance of being attracted by mouse
+            if(moveTowardMouse === 5){
+                this.directionX = mouseX - this.x > 0 ? 1 : -1;
+            } else if (moveTowardMouse === 1) {
+                this.directionY = mouseY - this.y > 0 ? 1 : -1;
+            }
 
-//   window.requestAnimationFrame(animation);
-// }
+            this.speed = 1.25; // Slow down
+        }
 
-// animation();
+        // Move bubbles
+        this.x += this.speed * this.directionX;
+        this.y += this.speed * this.directionY;
+
+        // Change radius
+        this.radius += changeSettings(this.radius, minSize, maxSize, 10);
+
+        // Draw the bubbles
+        ctx.save();
+        ctx.globalCompositeOperation = 'color-dodge';
+        ctx.beginPath();
+
+        var gradient = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.radius);
+        gradient.addColorStop(0, this.color.color1);
+        gradient.addColorStop(0.33, 'hsla(0,100%,10%,0)');
+        gradient.addColorStop(0.65, this.color.color2);
+        gradient.addColorStop(1, 'hsla(0,100%,10%,0)');
+
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+        ctx.fillStyle = gradient;
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+
+        // Pop bubbles if mouse coords match
+        if(pop === true && mouseOver(this.x,this.y,this.radius)) {
+            bubbles[this.id].destroy();
+            pop = false;
+        }
+
+    };
+
+    // Create initial bubbles
+    for (var i = 0; i < maxCount; i++) {
+        new Bubble();
+    }
+
+    // Call animation
+    var animate = function() {
+
+        // Clear canvas and fill with background color
+        ctx.fillStyle = bgcolor;
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        // Draw bubbles
+        for (var i = 1; i <= count; i++) {
+            bubbles[i].draw();
+        }
+
+        requestAnimationFrame(animate);
+
+    };
+
+    requestAnimationFrame(animate);
+
+    // Click to add new bubble
+    canvas.addEventListener('click',function(e){
+        new Bubble(e.pageX, e.pageY);
+    });
+
+    // Attract bubbles
+    var startAttracting;
+  
+    canvas.addEventListener('mousedown',function(e){
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+      
+        // Wait 0.5s before attracting bubbles
+        clearTimeout(startAttracting);
+        startAttracting = setTimeout(function(){
+            return attract = true;
+        }, 500);
+    });
+
+    // If mouse held down, update coords as the mouse moves
+    canvas.addEventListener('mousemove',function(e){
+        if(attract) {
+            mouseX = e.pageX;
+            mouseY = e.pageY;
+        }
+    });
+
+    // Clear attract
+    canvas.addEventListener('mouseup',function(e){
+        clearTimeout(startAttracting);
+        attract = false;
+    });
+  
+    // Resize canvas with window resize
+    var resizing;
+
+    window.addEventListener('resize', function(){
+        clearTimeout(resizing);
+        resizing = setTimeout(function(){
+          canvasWidth = canvas.width = window.innerWidth;
+          canvasHeight = canvas.height = window.innerHeight;
+        }, 500);
+    });
+
+}());
